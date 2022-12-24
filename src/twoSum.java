a2a0e81520359e223421e0ec1225a6fdca2bdbe6
s10jw <estonjw01@gmail.com>
2022-12-24 14:01:16 -0800

    twoSum

---
+++ src/twoSum.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class twoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++){
+                int a = nums[i] + nums[j];
+                if (a == target && i != j) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[1];
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 2, 4};
+        int targ = 6;
+        System.out.println(Arrays.toString(twoSum(nums, targ)));
+    }
+}
+
