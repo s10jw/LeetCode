a7546cf5e9f2a6e864869746c3bc0a4e169bbcf9
s10jw <estonjw01@gmail.com>
2022-12-28 18:50:19 -0800

    AddTwo

--- src/MediumProblems/AddTwo.java
+++ src/MediumProblems/AddTwo.java
@@ -1,37 +1,44 @@
 package MediumProblems;
 
 public class AddTwo {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int val = ListToInt(l1) + ListToInt(l2);
-        int length = FindLength(val);
-        int temp = val / (10^length);
-        length = length - 1;
-
-        ListNode res = new ListNode(temp);
-
-        while (length >= 0) {
-            res = new ListNode(val, res);
-
-            }
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        int node;
+        int val1;
+        int val2;
+        ListNode list = new ListNode();
+        while (l1 != null || l2 != null) {
+           if (l1 == null) { val1 = 0; } else { val1 = l1.val;}
+           if (l2 == null) { val2 = 0;} else { val2 = l2.val;}
+           node = val1 + val2 + carry;
+           if (node >= 10) {
+               carry = 1;
+               node = node - 10;
+               list = new ListNode(node, list);
+               System.out.println(node);
+               if (l1 != null) {l1 = l1.next;}
+               if (l2 != null) {l2 = l2.next;}
+           } else {
+               System.out.println(node);
+               list = new ListNode(node, list);
+               if (l1 != null) {l1 = l1.next;}
+               if (l2 != null) {l2 = l2.next;}
+               carry = 0;
+           }
         }
+        return ReverseLinkedList(list);
     }
-    private int ListToInt(ListNode list) {
-    int res = 0;
-    int val = 0;
-    while (list != null) {
-        res += list.val * (10^val);
-        list = list.next;
-        val++;
+    public static ListNode ReverseLinkedList(ListNode list) {
+        ListNode res = new ListNode();
+        while (list != null) {
+            res = new ListNode(list.val, res);
+            list = list.next;
         }
-    return res;
-    }
-    private int FindLength(int val) {
-        int length = 0;
-        long temp = 1;
-        while (temp <= val) {
-            length++;
-            temp = (temp << 3) + (temp << 1);
-        }
-        return length;
+        return res;
+    } public static void main (String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        System.out.println(addTwoNumbers(l1, l2));
     }
 }
+
