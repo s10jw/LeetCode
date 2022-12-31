32b7414a2d0f783f40dfc0131fffa987a57e4e11
s10jw <estonjw01@gmail.com>
2022-12-30 16:47:43 -0800

    AddTwo finished up! Wasn't used to using sentinel nodes, but as soon as I picked it up everything worked out.

--- src/MediumProblems/AddTwo.java
+++ src/MediumProblems/AddTwo.java
@@ -6,7 +6,8 @@ public class AddTwo {
         int node;
         int val1;
         int val2;
-        ListNode list = new ListNode();
+        ListNode sentinel = new ListNode();
+        ListNode list = sentinel;
         while (l1 != null || l2 != null) {
            if (l1 == null) { val1 = 0; } else { val1 = l1.val;}
            if (l2 == null) { val2 = 0;} else { val2 = l2.val;}
@@ -14,27 +15,34 @@ public class AddTwo {
            if (node >= 10) {
                carry = 1;
                node = node - 10;
-               list = new ListNode(node, list);
+               sentinel.next = new ListNode(node);
                System.out.println(node);
                if (l1 != null) {l1 = l1.next;}
                if (l2 != null) {l2 = l2.next;}
            } else {
                System.out.println(node);
-               list = new ListNode(node, list);
+               sentinel.next = new ListNode(node);
                if (l1 != null) {l1 = l1.next;}
                if (l2 != null) {l2 = l2.next;}
                carry = 0;
            }
+           sentinel = sentinel.next;
+        }
+        if (carry != 0) {
+            sentinel.next = new ListNode(carry);
+            sentinel = sentinel.next;
         }
         return ReverseLinkedList(list);
     }
     public static ListNode ReverseLinkedList(ListNode list) {
-        ListNode res = new ListNode();
+        ListNode sentinel = new ListNode();
+        ListNode res = sentinel;
         while (list != null) {
-            res = new ListNode(list.val, res);
+            sentinel.next = new ListNode(list.val);
             list = list.next;
+            sentinel = sentinel.next;
         }
-        return res;
+        return res.next;
     } public static void main (String[] args) {
         ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
         ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
